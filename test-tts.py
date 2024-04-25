1cd12ce4b540287f9d5a2b038249f7bbf2bd009b
Ibrahiem Jabareen <ibrahiem@nadsoft.net>
2024-04-26 00:58:17 +0300

    test

--- test-tts.py
+++ test-tts.py
@@ -2,7 +2,7 @@ from tortoise.api import TextToSpeech
 from tortoise.utils.audio import load_voice
 import torchaudio
 
-tts = TextToSpeech(kv_cache=True)
+tts = TextToSpeech(kv_cache=True, device="cuda")
 
 example_texts = ["مرحبا جميعاً كيف الحال؟",
                  "Hello where is the catch here?",
@@ -13,5 +13,6 @@ voice = "mona"
 
 for i, text in enumerate(example_texts):
     voice_samples, _ = load_voice(voice)
-    audio = tts.tts_with_preset(text, voice_samples=voice_samples, preset='fast')
+    print(tts.device)
+    audio = tts.tts_with_preset(text, voice_samples=voice_samples, preset='high_quality')
     torchaudio.save(f'{voice}-{i}.wav', audio.squeeze(0), 24000)
