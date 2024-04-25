80ef4a53e2847cb99ab9bc1a75b98f1f8d484034
Ibrahiem Jabareen <ibrahiem@nadsoft.net>
2024-04-26 00:51:11 +0300

    mona voice

--- test-tts.py
+++ test-tts.py
@@ -4,14 +4,14 @@ import torchaudio
 
 tts = TextToSpeech(kv_cache=True)
 
-example_texts = ["والجمهور مبسوط that's good",
-                 "اليوم، إذا أرخيصة ولا غالية ولا تراك قديم ولا ما بعرف he doesn't care هو بيسأل على الـ functionality أما هاي قصة أنه مستحيل، مست",
+example_texts = ["مرحبا جميعاً كيف الحال؟",
+                 "Hello where is the catch here?",
                  "كنت أنا أصغر واحدة البنت المدللة عند العيلة كلها يعني عند أمي وأبوي وأخوتي وإحنا عيلة كبيرة اسمالله"
                 ]
 
-voices = ["tom", "train_atkins", "angie"]
+voice = "mona"
 
-for voice, text in zip(voices, example_texts):
+for i, text in enumerate(example_texts):
     voice_samples, _ = load_voice(voice)
     audio = tts.tts_with_preset(text, voice_samples=voice_samples, preset='fast')
-    torchaudio.save(f'{voice}.wav', audio.squeeze(0), 24000)
+    torchaudio.save(f'{voice}-{i}.wav', audio.squeeze(0), 24000)
